f86437c243fc02c95d6f8b9baea623cbce86cc12
Nermin Memmedli <memmedlinermin205@gmail.com>
2025-10-09 00:06:06 +0400

    solve cors problem

--- src/main/java/com/dress/dressrenting/config/SecurityConfig.java
+++ src/main/java/com/dress/dressrenting/config/SecurityConfig.java
@@ -78,8 +78,11 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:5173"));
-        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowedOrigins(List.of(
+                "http://localhost:5173",
+                "http://localhost:8081",
+                "http://109.123.252.254"
+        ));        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
